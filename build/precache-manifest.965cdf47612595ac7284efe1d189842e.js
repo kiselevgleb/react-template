bd7aa32c7f24452c1c1f90795e61a302092b3190
kiselevgleb <kiselevgleb@gmail.com>
2020-09-02 12:49:36 +0300

    commit

---
+++ build/precache-manifest.965cdf47612595ac7284efe1d189842e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63dfb39792e98fd25d19c3ae00d380b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b02afed6d85c38785845",
+    "url": "/static/css/main.441607a3.chunk.css"
+  },
+  {
+    "revision": "e9343a8199fb4bcce912",
+    "url": "/static/js/2.3154457b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3154457b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b02afed6d85c38785845",
+    "url": "/static/js/main.c56fde34.chunk.js"
+  },
+  {
+    "revision": "e2c6e531b8324ac17d6c",
+    "url": "/static/js/runtime-main.206ac5b9.js"
+  },
+  {
+    "revision": "9c63a2c92146aa97904b9148bc055ce3",
+    "url": "/static/media/item-black.9c63a2c9.jpg"
+  },
+  {
+    "revision": "8bcbdffead0d5ba6700c1551189af1b8",
+    "url": "/static/media/item-blue.8bcbdffe.jpg"
+  }
+]);
